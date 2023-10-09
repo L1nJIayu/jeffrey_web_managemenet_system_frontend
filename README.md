0879a74109d5bff8bb8701d2cbf7fd1d35116f8e
L1nJIayu <562316920@qq.com>
2023-10-09 12:44:33 +0800

    vite+ts

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-
 # 后台管理系统-前端-开发记录
 
 
 ## 链接
+
 [后端代码](https://github.com/L1nJIayu/jeffrey_web_managemenet_system_backend)
+
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { useRoutes } from 'react-router-dom'
 import { default_routes } from './router/routes'
 
 
-
-
 const App = () => {
   return useRoutes(default_routes)
 }
